a2cefef8420779243dd4be0d513ef8bdcd601251
Bob <118316084+Wordsforausername@users.noreply.github.com>
2025-03-16 12:42:09 -0400

    UPDATE BACKEND (FIXED) - We botched the paramater name
    route(fetchRoomByAnyName) for getAnyRoom changed

--- app/backend/routes/roomRoutes.js
+++ app/backend/routes/roomRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const {fetchRooms,fetchRoomByName,fetchRoomByAnyName} = require("../controllers/roomController");
+const {
+  fetchRooms,
+  fetchRoomByName,
+  fetchRoomByAnyName,
+} = require("../controllers/roomController");
 
 const router = express.Router();
 
-router.get("/",fetchRooms);
-router.get("/official-name/:name",fetchRoomByName);
-router.get("/:nameOrAlias",fetchRoomByAnyName);
+router.get("/", fetchRooms);
+router.get("/official-name/:name", fetchRoomByName);
+router.get("/:name", fetchRoomByAnyName);
 
 module.exports = router;
